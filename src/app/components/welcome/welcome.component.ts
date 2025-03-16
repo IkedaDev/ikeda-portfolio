7feb872e19075ffc5cdf45e06f05381b5f6462f4
Ikeda <sebaaignacio111@gmail.com>
2025-03-15 21:01:59 -0300

    dev: se corrige animacion, se quita animate.css

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ScreenSizeService } from '../../services';
+import { InViewportDirective } from '@directives/in-viewport.directive';
 
 @Component({
   selector: 'app-welcome',
-  imports: [ ],
+  imports: [ InViewportDirective ],
   templateUrl: './welcome.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
---
+++ src/app/directives/in-viewport.directive.ts
@@ -0,0 +1,32 @@
+import { isPlatformServer } from '@angular/common';
+import { AfterViewInit, Directive, ElementRef, Inject, inject, input, PLATFORM_ID, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[inViewport]',
+})
+export class InViewportDirective implements AfterViewInit  { 
+
+  public readonly classToAdd = input<string>('visible')
+
+  private readonly el = inject(ElementRef)
+  private readonly renderer = inject(Renderer2)
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object){}
+
+  ngAfterViewInit(): void {
+    if(isPlatformServer(this.platformId)) return
+    const observer = new IntersectionObserver( entries => {
+      entries.forEach( entry => {
+
+        if( !entry.isIntersecting ) {
+          this.renderer.removeClass(this.el.nativeElement, this.classToAdd() )
+          return 
+        }
+
+        this.renderer.addClass(this.el.nativeElement, this.classToAdd() )
+      })
+    })
+    observer.observe(this.el.nativeElement)
+  }
+
+}
