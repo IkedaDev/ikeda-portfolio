0d628bafd3036b6f980b81a6b39e1cb5d0c77107
Ikeda Harket <44117850+IkedaHarket@users.noreply.github.com>
2025-12-29 14:38:26 -0300

    fix: change cv

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -13,7 +13,7 @@ export class WelcomeComponent {
   public readonly screenSizeService = inject(ScreenSizeService)
 
   openCV(){
-    open('https://drive.google.com/file/d/1K0kb-xHwahGxZg-nVpuayWYV_DMNsw25/view?usp=sharing','__blank')
+    open('https://drive.google.com/file/d/1Fo57MNk1iigRmAbPNjSOjJhFj7f0mel2/view?usp=sharing','__blank')
   }
 
 }
